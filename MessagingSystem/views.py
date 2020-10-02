328bfa95207aa9902e7b1a90799b67b302f6bbb1
PAZJR93 <omerpaz93@gmail.com>
2020-10-02 11:42:56 +0300

    fixing convert to json obj

--- MessagingSystem/views.py
+++ MessagingSystem/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render , redirect
 from MessagingSystem.models import Message
 from django.contrib.auth.models import User
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
+from django.forms.models import model_to_dict
+
 import json
 
-
+@csrf_exempt
 def write_message(request):
     """
     write a new message by user to other user
@@ -26,24 +27,28 @@ def write_message(request):
     -------
     None
     """ 
-    if request.method == 'GET':
+    if request.method == 'POST':
 
+        # get request info from the body.
+        sender = request.POST.get('sender')
+        receiver = request.POST.get('receiver')
+        msg = request.POST.get('msg')
+        subject = request.POST.get('subject')
         # convert the sender and the reciver names to a user objects.
-        
-        sender_user_Obj = User.objects.filter(username="omer").first()
-        receiver_user_Obj = User.objects.filter(username="tal").first()
+        sender_user_Obj = User.objects.filter(username=sender).first()
+        receiver_user_Obj = User.objects.filter(username=receiver).first()
 
-        #Create a new message and save in db
-        msg = Message(sender=sender_user_Obj,receiver=receiver_user_Obj,message="here",subject="test")
+        # #Create a new message and save in db
+        msg = Message(sender=sender_user_Obj,receiver=receiver_user_Obj,message=msg,subject=subject)
         msg.save()
-        a = Message.objects.filter(id=msg.id).first()
+        # convert model object to a json
+        dict_obj = model_to_dict(msg)
+        serialized = json.dumps(dict_obj)
+        return HttpResponse(serialized, content_type='application/json')
 
 
-        return JsonResponse(list,safe=False)
-        # return JsonResponse({"models_to_return": list(msg)}, safe=False)
-    # return JsonResponse({"models_to_return": list(msg)},safe=False)
 
-
+@csrf_exempt
 def get_all_messages(request):
     """
     Receive all messages for a specific user
@@ -57,24 +62,28 @@ def get_all_messages(request):
     -------
     QuerySet
         a QuerySet that contains all the messages of the specific user
-    """ 
-
+    """
     if request.method == 'POST':
         # List of messages of the requested user
         user_messages = []
-        user_post_option = request.POST.get('paz',False)
+        user_name = request.POST.get('user_name')
+        print(request.GET)
         # convert the user name to a user objects.
-        user_Obj = User.objects.filter(username=user_post_option).first()
+        user_Obj = User.objects.filter(username=user_name).first()
         # Get all the messages on th
 
         messages_for_specific_user = Message.objects.values_list().filter(receiver=user_Obj.id)
-        return JsonResponse(list(messages_for_specific_user),safe=False)
+        qs_json = serializers.serialize('json', messages_for_specific_user)
+        return HttpResponse(qs_json, content_type='application/json')
 
+@csrf_exempt
 def get_all_unread_messages(request):
     pass
 
+@csrf_exempt
 def read_message(request):
     pass
 
+@csrf_exempt
 def delete_message(request):
     pass
--- ex.py
+++ ex.py
@@ -4,11 +4,21 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MessagingSite.settings')
 django.setup()
 from MessagingSystem.models import Message
 from django.contrib.auth.models import User
-
-
+import json
+from django.http import JsonResponse,HttpResponse
+from django.core import serializers
 
 
 if __name__ == '__main__':
-    a = Message.objects.values_list().filter(receiver=2)
-    print(a)
+    user_Obj = User.objects.filter(username="omer").first()
+        # Get all the messages on th
+
+    messages_for_specific_user = Message.objects.values_list().filter(receiver=user_Obj.id)
+    print(messages_for_specific_user)
+    # user_Obj = User.objects.filter(username="nitzan").first()
+    # # Get all the messages on th
+
+    # messages_for_specific_user = Message.objects.values_list().filter(receiver=user_Obj.id)
+
+    # print(json.dumps(messages_for_specific_user, indent=4)
 
