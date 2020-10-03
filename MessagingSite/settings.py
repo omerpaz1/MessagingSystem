58ac61b2e243dd04f7faf9aa255de76b1c5e3005
PAZJR93 <omerpaz93@gmail.com>
2020-10-03 18:35:56 +0300

    update static

--- MessagingSite/settings.py
+++ MessagingSite/settings.py
@@ -29,7 +29,10 @@ ALLOWED_HOSTS = ['msg-sys.herokuapp.com']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
 # Application definition
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
