b51216db1558f62f8c0de1f51e930908463376e6
PAZJR93 <omerpaz93@gmail.com>
2020-10-03 20:20:13 +0300

    return the site

--- MessagingSite/urls.py
+++ MessagingSite/urls.py
@@ -20,7 +20,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('MessagingSystem.urls')),
 ]
-if not settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
