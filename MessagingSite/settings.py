ed18c075473abfe39180c55587ba9c78f72fa907
PAZJR93 <omerpaz93@gmail.com>
2020-10-03 18:04:43 +0300

    update staticfiles

--- MessagingSite/settings.py
+++ MessagingSite/settings.py
@@ -27,8 +27,13 @@ DEBUG = False
 ALLOWED_HOSTS = ['msg-sys.herokuapp.com']
 
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # Application definition
