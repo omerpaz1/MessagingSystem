b9b99f6e67fcf8b1b400556330380135f831c9d3
PAZJR93 <omerpaz93@gmail.com>
2020-10-04 15:31:54 +0300

    adding forms

--- MessagingSite/settings.py
+++ MessagingSite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'MessagingSystem.apps.MessagingsystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
-    'rest_framework',
+    'django.rest_framework.authtoken',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
