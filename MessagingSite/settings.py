77e8396f82d3d20845e9372934caac6ffd1cb5b1
PAZJR93 <omerpaz93@gmail.com>
2020-10-04 16:06:31 +0300

    try fix

--- MessagingSite/settings.py
+++ MessagingSite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,8 +28,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['msg-sys.herokuapp.com']
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
 
 
 
